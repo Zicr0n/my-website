44f78ca6117fb4a2f04e477795f977560cb7fc0d
Zicr0n <alwanisamir007@gmail.com>
2024-10-17 10:15:03 +0200

    yesss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <main>
 	<div>
   
-	  <h1> Samir </h1>
+	  <h1> SMamme </h1>
 	  <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 	  <h3>Inspirerade citat för min webbutvecklingsresa:</h3>
 	  <h7>I HATE TREES!!!</h7>
