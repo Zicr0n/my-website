a485b4aa133a5cb99cecc1c0716e4495014225bd
Zicr0n <alwanisamir007@gmail.com>
2025-01-12 17:05:19 +0100

    potential fix for prerender issue

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,10 @@
+<form on:submit|preventDefault={()=> goto('/search/'+ search)}>
+    <input type="text" placeholder="Sök upp en pokemon" bind:value={search} />
+</form>
+
+
+<script>
+    export let search;
+
+    import { goto } from '$app/navigation';
+</script>
